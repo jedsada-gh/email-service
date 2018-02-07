2ef9a7257f0a243fa3935a030566989bae3ec0de
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-02-07 15:12:28 +0700

    add mailgun doamin sandbox

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	mailgun "github.com/mailgun/mailgun-go"
+)
 
 func main() {
-	fmt.Println("Hello E-mail Service")
+	domain := os.Getenv("DOMAIN_MAILGUN_SANDBOX")
+	apiKeyPrivate := os.Getenv("PRIVATE_KEY_MAILGUN")
+	apiKeyPublic := os.Getenv("PUBLIC_KEY_MAILGUN")
+	mg := mailgun.NewMailgun(domain, apiKeyPrivate, apiKeyPublic)
+	message := mg.NewMessage(
+		"jt@20scoops.net",
+		"Fancy subject!",
+		"",
+		"pondthaitay@gmail.com")
+	html, err := ioutil.ReadFile("templates/inlined/alert.html")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	message.SetHtml(string(html))
+	_, _, err = mg.Send(message)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("send email successfully")
+	}
 }
